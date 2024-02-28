17e99d2318ee222ea6d01138c7923b78d9d0f763
sensehae <106131457+sehae@users.noreply.github.com>
2024-02-28 09:28:09 +0800

    changed naming

--- styles/universalStyles.py
+++ styles/universalStyles.py
@@ -1,7 +1,14 @@
-HEADER_STYLE = """
+HEADER_TITLE = """
 QLabel {
     color: #67B99A;
     font-size: 45px;
     padding: 10px;
+}
+"""
+
+SYSTEM_LABEL = """
+QLabel {
+    font-size: 15px;
+    padding: 0px;
 }
 """
