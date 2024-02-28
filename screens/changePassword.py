ac90221ba523bf779464251d97d6cb1803bd78d8
sensehae <106131457+sehae@users.noreply.github.com>
2024-02-28 09:27:43 +0800

    Update changePassword.py

--- screens/changePassword.py
+++ screens/changePassword.py
@@ -9,15 +9,17 @@ from PyQt5.QtWidgets import (
     QFrame,
     QLabel,
     QScrollArea,
-    QSizePolicy,
+    QSizePolicy, QPushButton,
 )
+from PyQt5.QtGui import QFontDatabase, QFont
+from styles.universalStyles import HEADER_TITLE, SYSTEM_LABEL
 
-from styles.universalStyles import HEADER_STYLE
-
+QUICKSAND = "assets/Quicksand-Regular.ttf"
 
 class ChangePasswordUI(QWidget):
     def __init__(self):
         super().__init__()
+        QFontDatabase.addApplicationFont(QUICKSAND)
 
         # Header Contents
         self.frame1 = self.setupTitleFrame()
@@ -61,7 +63,9 @@ class ChangePasswordUI(QWidget):
         layout = QHBoxLayout(frame)
 
         content_label1 = QLabel("CHANGE PASSWORD")
-        content_label1.setStyleSheet(HEADER_STYLE)
+        content_label1.setStyleSheet(HEADER_TITLE)
+        content_label1.setFont(QFont("Quicksand"))
+
         layout.addWidget(content_label1)
 
         return frame
@@ -74,12 +78,21 @@ class ChangePasswordUI(QWidget):
         layout = QVBoxLayout(frame)
 
         self.system_time_label = QLabel()
+        self.system_time_label.setStyleSheet(SYSTEM_LABEL)
         self.updateTime()
         self.timer = QTimer(self)
         self.timer.timeout.connect(self.updateTime)
         self.timer.start(1000)
 
-        layout.addWidget(self.system_time_label)
+        layout.addWidget(self.system_time_label, alignment=Qt.AlignRight)
+
+        if hasattr(self, 'system_time_label'):
+            username = QLabel("User's Name Placeholder")
+            username.setStyleSheet(SYSTEM_LABEL)
+
+            layout.addWidget(username, alignment=Qt.AlignRight)
+
+
         return frame
 
     def updateTime(self):
@@ -93,9 +106,24 @@ class ChangePasswordUI(QWidget):
         frame.setStyleSheet("background-color: red;")
         frame.setFixedWidth(100)
 
-        layout = QHBoxLayout(frame)
+        layout = QVBoxLayout(frame)
+        layout.setAlignment(Qt.AlignCenter)
+
+        button1 = QPushButton("Button 1", self)
+        button2 = QPushButton("Button 2", self)
+
+        button1.clicked.connect(self.on_button_click)
+        button2.clicked.connect(self.on_button_click)
+
+        layout.addWidget(button1)
+        layout.addWidget(button2)
+
+        layout.setSpacing(0)
 
         return frame
+
+    def on_button_click(self):
+        print("Button clicked!")
 
     def setupContentFrame(self):
         frame = QFrame(self)
@@ -116,6 +144,8 @@ class ChangePasswordUI(QWidget):
 class ChangePassword(QMainWindow):
     def __init__(self):
         super().__init__()
+
+        QFontDatabase.addApplicationFont(QUICKSAND)
 
         self.setGeometry(100, 100, 800, 600)
         self.showFullScreen()
