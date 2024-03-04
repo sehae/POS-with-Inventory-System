458d857fac314e4d9dbe184c0d5f6c51b34fa27d
sensehae <106131457+sehae@users.noreply.github.com>
2024-03-04 19:51:28 +0800

    Update universalStyles.py

--- styles/universalStyles.py
+++ styles/universalStyles.py
@@ -12,3 +12,20 @@ QLabel {
     padding: 0px;
 }
 """
+
+NAVBAR_BUTTON_STYLE = """
+QPushButton {
+    background-color: #FFFFFF;
+    color: black;
+    border: 1.3px solid #67B99A;
+    border-radius: 5px;
+}
+QPushButton:hover {
+    background-color: #E5F7EF;
+    border: 1px solid #4D926D;
+}
+QPushButton:pressed {
+    background-color: #C2E1D2;
+    border: 1px solid #265C42;
+    }
+"""
