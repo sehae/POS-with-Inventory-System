0010ec6a99bf129700e3b40013bfc623abdd78cd
sensehae <106131457+sehae@users.noreply.github.com>
2024-06-01 18:31:55 +0800

    working OTP

--- styles/universalStyles.py
+++ styles/universalStyles.py
@@ -1,3 +1,65 @@
+HEADER_TITLE = """
+QLabel {
+    color: #67B99A;
+    font-size: 45px;
+    padding: 10px;
+}
+"""
+
+SYSTEM_LABEL = """
+QLabel {
+    font-size: 15px;
+    padding: 0px;
+}
+"""
+
+NAVBAR_BUTTON_STYLE = """
+QPushButton {
+    background-color: #FFFFFF;
+    color: black;
+    border: 1.3px solid #67B99A;
+    border-radius: 5px;
+}
+QPushButton:hover {
+    background-color: #E5F7EF;
+    border: 1px solid #4D926D;
+}
+QPushButton:pressed {
+    background-color: #C2E1D2;
+    border: 1px solid #265C42;
+    }
+"""
+
+ACTIVE_BUTTON_STYLE = """
+QPushButton {
+    background-color: #67B99A;
+    color: white;
+    padding: 8px 16px;
+    border-radius: 5px;
+}
+QPushButton:hover {
+    background-color: #5CAE8B;
+}
+QPushButton:pressed {
+    background-color: #4D9C7F;
+}
+"""
+
+INACTIVE_BUTTON_STYLE = """
+QPushButton {
+    background-color: #9D9D9D;
+    color: white;
+    padding: 8px 16px;
+    border-radius: 5px;
+}
+QPushButton:hover {
+    background-color: #7A7A7A;
+}
+QPushButton:pressed {
+    background-color: #666666;
+}
+"""
+
 ENABLED_RESEND_BTN = """
 QPushButton {
     padding: 5px;
