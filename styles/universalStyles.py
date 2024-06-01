096f74ca80e84b8c0eb188cc01bdc65d8a00ed87
sensehae <106131457+sehae@users.noreply.github.com>
2024-06-01 18:25:01 +0800

    Update universalStyles.py

--- styles/universalStyles.py
+++ styles/universalStyles.py
@@ -1,62 +1,37 @@
-HEADER_TITLE = """
-QLabel {
-    color: #67B99A;
-    font-size: 45px;
-    padding: 10px;
-}
-"""
-
-SYSTEM_LABEL = """
-QLabel {
-    font-size: 15px;
-    padding: 0px;
-}
-"""
-
-NAVBAR_BUTTON_STYLE = """
+ENABLED_RESEND_BTN = """
 QPushButton {
-    background-color: #FFFFFF;
-    color: black;
-    border: 1.3px solid #67B99A;
-    border-radius: 5px;
+    padding: 5px;
+    border: 2px Solid #036666;
+    border-radius: 6px;
+    background-color: transparent;
+    color: #036666;
+    font-size: 20px;
 }
 QPushButton:hover {
-    background-color: #E5F7EF;
-    border: 1px solid #4D926D;
+    border-color: #049393;
+    color: #049393;
 }
 QPushButton:pressed {
-    background-color: #C2E1D2;
-    border: 1px solid #265C42;
-    }
+    border-color: #024c4c;
+    color: #024c4c;
+}
 """
 
-ACTIVE_BUTTON_STYLE = """
+DISABLED_RESEND_BTN = """
 QPushButton {
-    background-color: #67B99A;
-    color: white;
-    padding: 8px 16px;
-    border-radius: 5px;
+    padding: 5px;
+    border: 2px solid #A9A9A9;
+    border-radius: 6px;
+    background-color: transparent;
+    color: #A9A9A9;
+    font-size: 20px;
 }
 QPushButton:hover {
-    background-color: #5CAE8B;
+    border-color: #A9A9A9;
+    color: #A9A9A9;
 }
 QPushButton:pressed {
-    background-color: #4D9C7F;
+    border-color: #A9A9A9;
+    color: #A9A9A9;
 }
 """
-
-INACTIVE_BUTTON_STYLE = """
-QPushButton {
-    background-color: #9D9D9D;
-    color: white;
-    padding: 8px 16px;
-    border-radius: 5px;
-}
-QPushButton:hover {
-    background-color: #7A7A7A;
-}
-QPushButton:pressed {
-    background-color: #666666;
-}
-"""
-
