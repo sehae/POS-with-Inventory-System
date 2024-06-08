ecde65e228fc7c166b0fa881f9fe0761b0df6a07
sensehae <106131457+sehae@users.noreply.github.com>
2024-06-08 15:17:36 +0800

    Update login_functions.py

--- screens/authentication_screens/login_screen/login_functions.py
+++ screens/authentication_screens/login_screen/login_functions.py
@@ -1,12 +1,4 @@
-from PyQt5 import QtWidgets, QtGui
-from PyQt5.QtGui import QIcon
-
-from screens.authentication_screens.login_screen.loginScreen import Ui_MainWindow
-from screens.admin_screens.admin_dashboard.adminDashboard_functions import myAdminDashboard
-from security.hash import verify_password
-from shared.dialog import show_error_message
-from server.local_server import conn
-
+from shared.imports import *
 
 class myLoginScreen(Ui_MainWindow):
     def __init__(self):
@@ -43,10 +35,7 @@ class myLoginScreen(Ui_MainWindow):
             cursor = conn.cursor()
 
             # Query the adminlogin table
-            cursor.execute(
-                "SELECT admin_id, password FROM adminlogin WHERE username = %s",
-                (username,)
-            )
+            cursor.execute(GET_ADMIN_LOGIN, (username,))
             result = cursor.fetchone()
 
             if result:
@@ -54,8 +43,7 @@ class myLoginScreen(Ui_MainWindow):
 
                 # Verify the provided password against the stored password
                 if verify_password(stored_password, provided_password):
-                    fetch_query = "SELECT first_name FROM admin WHERE admin_id = %s"
-                    cursor.execute(fetch_query, (admin_id,))
+                    cursor.execute(GET_ADMIN_FIRST_NAME, (admin_id,))
                     admin_first_name = cursor.fetchone()[0]
                     print(f"Login successful as admin: Welcome {admin_first_name}!")
                     self.admin_dashboard.open_admin_dashboard()
@@ -64,10 +52,7 @@ class myLoginScreen(Ui_MainWindow):
                     print("Incorrect password.")
 
             # Query the employeelogin table
-            cursor.execute(
-                "SELECT employee_id, password FROM employeelogin WHERE username = %s",
-                (username,)
-            )
+            cursor.execute(GET_EMPLOYEE_LOGIN, (username,))
             result = cursor.fetchone()
 
             if result:
@@ -75,8 +60,7 @@ class myLoginScreen(Ui_MainWindow):
 
                 # Verify the provided password against the stored password
                 if verify_password(stored_password, provided_password):
-                    fetch_query = "SELECT first_name FROM employee WHERE employee_id = %s"
-                    cursor.execute(fetch_query, (employee_id,))
+                    cursor.execute(GET_EMPLOYEE_FIRST_NAME, (employee_id,))
                     employee_first_name = cursor.fetchone()[0]
                     print(f"Login successful as Employee: Welcome {employee_first_name}!")
                     return
