720b0b6678a4a0ef6fe24fb152095c67c4b077cb
sensehae <106131457+sehae@users.noreply.github.com>
2024-06-08 16:18:53 +0800

    Update imports.py

--- shared/imports.py
+++ shared/imports.py
@@ -8,14 +8,17 @@ import string
 # PyQt5 imports
 from PyQt5 import QtWidgets, QtGui, QtCore
 from PyQt5.QtGui import QIcon, QRegExpValidator
-from PyQt5.QtWidgets import QLineEdit, QAction, QApplication, QMainWindow
+from PyQt5.QtWidgets import QLineEdit, QAction, QApplication, QMainWindow, QMessageBox
 from PyQt5.QtCore import QRegExp
 
 # Database
 from server.local_server import conn
 from database.DB_Queries import (GET_ADMIN_LOGIN, GET_ADMIN_FIRST_NAME, GET_EMPLOYEE_LOGIN,
                                  GET_EMPLOYEE_FIRST_NAME, GET_ADMIN_ID, GET_EMPLOYEE_ID, UPDATE_ADMIN_PASSWORD,
-                                 UPDATE_EMPLOYEE_PASSWORD, ADD_ADMIN, ADD_EMPLOYEE, ADD_ADMIN_LOGIN, ADD_EMPLOYEE_LOGIN)
+                                 UPDATE_EMPLOYEE_PASSWORD, ADD_ADMIN, ADD_EMPLOYEE, ADD_ADMIN_LOGIN, ADD_EMPLOYEE_LOGIN,
+                                 GET_ADMIN_DATA, GET_EMPLOYEE_DATA, MOVE_TO_ADMIN, MOVE_TO_EMPLOYEE,
+                                 UPDATE_EMPLOYEE_DEPARTMENT, ENABLE_ADMIN, ENABLE_EMPLOYEE, DISABLE_ADMIN,
+                                 DISABLE_EMPLOYEE, SEARCH_EMPLOYEE, SEARCH_ADMIN,)
 
 # Automated Email
 from automated.email_automation import send_username_password
@@ -28,15 +31,17 @@ from validator.password_validator import isValidPassword
 from validator.internet_connection import is_connected
 
 # Shared
-from shared.dialog import show_error_message, show_username_password
+from shared.dialog import (show_error_message, show_username_password, confirmation_dialog, create_dialog_box)
 
 # Styles
-from styles.universalStyles import COMBOBOX_STYLE, COMBOBOX_STYLE_VIEW, COMBOBOX_DISABLED_STYLE
+from styles.universalStyles import (COMBOBOX_STYLE, COMBOBOX_STYLE_VIEW, COMBOBOX_DISABLED_STYLE,
+                                    ACTIVE_BUTTON_STYLE, INACTIVE_BUTTON_STYLE, )
 
 # Authentication Screens
 from screens.authentication_screens.login_screen.loginScreen import Ui_MainWindow
 from screens.authentication_screens.password_recovery.passwordRecovery import Ui_MainWindow
 from screens.authentication_screens.login_screen.login_functions import myLoginScreen
 
-# Dashboard
+# Admin Screen
+from screens.admin_screens.admin_maintenance.maintenanceEDIT import Ui_MainWindow
 from screens.admin_screens.admin_dashboard.adminDashboard_functions import myAdminDashboard
