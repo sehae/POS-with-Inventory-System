d6057a9901aee02233fa59decb404c6ccfac600e
ImNotKrakeN <ymnwlj@gmail.com>
2024-06-13 21:22:25 +0800

    Connection of POS Module

--- main.py
+++ main.py
@@ -1,25 +1,9 @@
 import sys
 from PyQt5 import QtWidgets
-
-from screens.authentication_screens.login_screen.login_functions import myLoginScreen
-from screens.admin_screens.admin_dashboard.adminDashboard_functions import myAdminDashboard
-from screens.admin_screens.admin_maintenance.m_ADDuser_functions import adminMaintenance
-from screens.admin_screens.admin_maintenance.m_EDITuser_functions import adminMaintenanceEDIT
-from screens.admin_screens.admin_inventory.inventoryAddProduct_functions import adminInventoryAddProduct
-from screens.admin_screens.admin_inventory.inventoryModify_functions import adminInventoryModifyProduct
-from screens.about_screen.about_devCredits_functions import aboutdevCredits
-from screens.about_screen.about_Info_functions import aboutInfo
-from screens.help_screen.help_FAQ_functions import helpFAQ
-from screens.help_screen.help_support_functions import helpSupport
-from screens.help_screen.help_usermanual_functions import helpManual
-from screens.password_screen.changePassword_functions import changePassword
-
-from screens.employee_screens.employee_dashboard.employeeDashboard_functions import myEmployeeDashboard
 from screens.employee_screens.employee_pos.posOrder_functions import posOrder
 from screens.employee_screens.employee_pos.posPayment_functions import posPayment
 from screens.employee_screens.employee_pos.posTable_functions import posTable
-from screens.employee_screens.employee_inventory.inventory_Modify_functions import inventoryModify
-from screens.employee_screens.employee_inventory.inventory_Barcode_functions import inventoryBarcode
+from screens.employee_screens.employee_pos.posHome_functions import posHome
 
 class MainWindow(QtWidgets.QMainWindow):
     def __init__(self):
@@ -31,145 +15,31 @@ class MainWindow(QtWidgets.QMainWindow):
         self.stacked_widget = QtWidgets.QStackedWidget()
         self.setCentralWidget(self.stacked_widget)
 
-        self.login_screen = myLoginScreen()
-        self.admin_dashboard = myAdminDashboard()
-        self.admin_maintenance = adminMaintenance()
-        self.admin_maintenanceEDIT = adminMaintenanceEDIT()
-        self.admin_inventory = adminInventoryAddProduct()
-        self.admin_inventoryMODIFY = adminInventoryModifyProduct()
-        self.about_devCredits = aboutdevCredits()
-        self.about_info = aboutInfo()
-        self.help_FAQ = helpFAQ()
-        self.help_support = helpSupport()
-        self.help_manual = helpManual()
-        self.change_password = changePassword()
-
-        self.employee_dashboard = myEmployeeDashboard()
         self.pos_order = posOrder()
         self.pos_payment = posPayment()
         self.pos_table = posTable()
-        self.inventory_modify = inventoryModify()
-        self.inventory_barcode = inventoryBarcode()
+        self.pos_home = posHome()
 
-        self.stacked_widget.addWidget(self.login_screen)
-        self.stacked_widget.addWidget(self.admin_dashboard)
-        self.stacked_widget.addWidget(self.admin_maintenance)
-        self.stacked_widget.addWidget(self.admin_maintenanceEDIT)
-        self.stacked_widget.addWidget(self.admin_inventory)
-        self.stacked_widget.addWidget(self.admin_inventoryMODIFY)
-        self.stacked_widget.addWidget(self.about_devCredits)
-        self.stacked_widget.addWidget(self.about_info)
-        self.stacked_widget.addWidget(self.help_FAQ)
-        self.stacked_widget.addWidget(self.help_support)
-        self.stacked_widget.addWidget(self.help_manual)
-        self.stacked_widget.addWidget(self.change_password)
-        self.stacked_widget.addWidget(self.employee_dashboard)
-        self.stacked_widget.addWidget(self.pos_order)
+        self.stacked_widget.addWidget(self.pos_table)  # pos_table is now the home screen
+        self.stacked_widget.addWidget(self.pos_home)   # pos_home is now the menu screen
         self.stacked_widget.addWidget(self.pos_payment)
-        self.stacked_widget.addWidget(self.pos_table)
-        self.stacked_widget.addWidget(self.inventory_modify)
-        self.stacked_widget.addWidget(self.inventory_barcode)
+        self.stacked_widget.addWidget(self.pos_order)
 
-        self.login_screen.login_successful.connect(self.show_admin_dashboard)
-        self.admin_dashboard.logout_signal.connect(self.show_login_screen)
-        self.admin_dashboard.maintenance_signal.connect(self.show_admin_maintenance)
-        self.admin_dashboard.about_signal.connect(self.show_about_devcredits)
-        self.admin_maintenance.edit_signal.connect(self.show_admin_maintenance_edit)
-        self.admin_maintenanceEDIT.add_signal.connect(self.show_admin_maintenance)
-        self.admin_maintenanceEDIT.back_signal.connect(self.show_admin_dashboard)
-        self.admin_maintenance.back_signal.connect(self.show_admin_dashboard)
-        self.admin_dashboard.inventory_signal.connect(self.show_admin_inventory)
-        self.admin_inventory.back_signal.connect(self.show_admin_dashboard)
-        self.admin_inventory.modify_signal.connect(self.show_admin_inventory_modify)
-        self.admin_inventoryMODIFY.add_signal.connect(self.show_admin_inventory)
-        self.admin_inventoryMODIFY.back_signal.connect(self.show_admin_dashboard)
-        self.about_devCredits.back_signal.connect(self.show_admin_dashboard)
-        self.about_devCredits.info_signal.connect(self.show_about_info)
-        self.about_info.back_signal.connect(self.show_admin_dashboard)
-        self.about_info.credits_signal.connect(self.show_about_devcredits)
-        self.admin_dashboard.help_signal.connect(self.show_help_faq)
-        self.help_FAQ.support_signal.connect(self.show_help_support)
-        self.help_FAQ.back_signal.connect(self.show_admin_dashboard)
-        self.help_FAQ.back_employee_signal.connect(self.show_employee_dashboard)
-        self.help_FAQ.manual_signal.connect(self.show_user_manual)
-        self.help_support.back_signal.connect(self.show_admin_dashboard)
-        self.help_support.manual_signal.connect(self.show_user_manual)
-        self.help_support.faq_signal.connect(self.show_help_faq)
-        self.help_manual.support_signal.connect(self.show_help_support)
-        self.help_manual.back_signal.connect(self.show_admin_dashboard)
-        self.help_manual.faq_signal.connect(self.show_help_faq)
-        self.admin_dashboard.changepass_signal.connect(self.show_change_password)
-        self.change_password.back_signal.connect(self.show_admin_dashboard)
-
-        self.help_manual.back_employee_signal.connect(self.show_employee_dashboard)
-        self.help_support.back_employee_signal.connect(self.show_employee_dashboard)
-
-        self.login_screen.login_successful_employee.connect(self.show_employee_dashboard)
-        self.employee_dashboard.pos_signal.connect(self.show_pos_order)
-        self.employee_dashboard.logout_signal.connect(self.show_login_screen)
-        self.pos_order.back_signal.connect(self.show_employee_dashboard)
+        # Connect signals to change screens within MainWindow
         self.pos_order.payment_signal.connect(self.show_pos_payment)
         self.pos_order.menu_signal.connect(self.show_pos_menu)
-
-        self.pos_payment.back_signal.connect(self.show_employee_dashboard)
-        self.pos_payment.menu_signal.connect(self.show_pos_menu)
+        self.pos_order.home_signal.connect(self.show_pos_home)
+        self.pos_payment.home_signal.connect(self.show_pos_home)        # Connect posPayment's home_signal
+        self.pos_payment.menu_signal.connect(self.show_pos_menu)        # Connect posPayment's menu_signal
         self.pos_payment.order_signal.connect(self.show_pos_order)
         self.pos_table.payment_signal.connect(self.show_pos_payment)
-        self.pos_table.back_signal.connect(self.show_employee_dashboard)
         self.pos_table.order_signal.connect(self.show_pos_order)
+        self.pos_table.menu_signal.connect(self.show_pos_menu)
+        self.pos_home.payment_signal.connect(self.show_pos_payment)
+        self.pos_home.order_signal.connect(self.show_pos_order)
+        self.pos_home.home_signal.connect(self.show_pos_home)
+        self.pos_home.back_signal.connect(self.show_pos_home)
 
-        self.employee_dashboard.inventoryModify_signal.connect(self.show_employee_inventory)
-        self.inventory_modify.barcode_signal.connect(self.show_inventory_barcode)
-        self.inventory_modify.back_signal.connect(self.show_employee_dashboard)
-        self.inventory_barcode.back_signal.connect(self.show_employee_dashboard)
-        self.inventory_barcode.modify_signal.connect(self.show_employee_inventory)
-        self.employee_dashboard.help_signal.connect(self.show_help_faq)
-
-        self.employee_dashboard.about_signal.connect(self.show_about_devcredits)
-        self.about_devCredits.back_employee_signal.connect(self.show_employee_dashboard)
-        self.about_info.back_employee_signal.connect(self.show_employee_dashboard)
-        self.employee_dashboard.changepass_signal.connect(self.show_change_password)
-        self.change_password.back_employee_signal.connect(self.show_employee_dashboard)
-
-
-    def show_login_screen(self):
-        self.stacked_widget.setCurrentWidget(self.login_screen)
-
-    def show_admin_dashboard(self):
-        self.stacked_widget.setCurrentWidget(self.admin_dashboard)
-
-    def show_admin_maintenance(self):
-        self.stacked_widget.setCurrentWidget(self.admin_maintenance)
-
-    def show_admin_maintenance_edit(self):
-        self.stacked_widget.setCurrentWidget(self.admin_maintenanceEDIT)
-
-    def show_admin_inventory(self):
-        self.stacked_widget.setCurrentWidget(self.admin_inventory)
-
-    def show_admin_inventory_modify(self):
-        self.stacked_widget.setCurrentWidget(self.admin_inventoryMODIFY)
-
-    def show_about_devcredits(self):
-        self.stacked_widget.setCurrentWidget(self.about_devCredits)
-
-    def show_about_info(self):
-        self.stacked_widget.setCurrentWidget(self.about_info)
-
-    def show_help_faq(self):
-        self.stacked_widget.setCurrentWidget(self.help_FAQ)
-
-    def show_help_support(self):
-        self.stacked_widget.setCurrentWidget(self.help_support)
-
-    def show_user_manual(self):
-        self.stacked_widget.setCurrentWidget(self.help_manual)
-
-    def show_change_password(self):
-        self.stacked_widget.setCurrentWidget(self.change_password)
-
-    def show_employee_dashboard(self):
-        self.stacked_widget.setCurrentWidget(self.employee_dashboard)
 
     def show_pos_order(self):
         self.stacked_widget.setCurrentWidget(self.pos_order)
@@ -177,17 +47,11 @@ class MainWindow(QtWidgets.QMainWindow):
     def show_pos_payment(self):
         self.stacked_widget.setCurrentWidget(self.pos_payment)
 
-    def show_pos_menu(self):
+    def show_pos_home(self):
         self.stacked_widget.setCurrentWidget(self.pos_table)
 
-    def show_employee_inventory(self):
-        self.stacked_widget.setCurrentWidget(self.inventory_modify)
-
-    def show_inventory_barcode(self):
-        self.stacked_widget.setCurrentWidget(self.inventory_barcode)
-
-
-
+    def show_pos_menu(self):
+        self.stacked_widget.setCurrentWidget(self.pos_home)  # Switch to pos_home (menu screen)
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
--- screens/employee_screens/employee_pos/posTable_functions.py
+++ screens/employee_screens/employee_pos/posTable_functions.py
@@ -1,49 +1,171 @@
-from PyQt5 import QtWidgets, QtCore
-from PyQt5.QtWidgets import QMessageBox
-from PyQt5.QtCore import QDateTime, QTimer, Qt
-from PyQt5.QtWidgets import QMainWindow
+import sys
+from PyQt5 import QtCore, QtGui, QtWidgets
 from screens.employee_screens.employee_pos.posTable import Ui_MainWindow
-from styles.universalStyles import ACTIVE_BUTTON_STYLE, INACTIVE_BUTTON_STYLE
-from server.local_server import conn
 
-class posTable(QMainWindow, Ui_MainWindow):
+
+
+class posTable(QtWidgets.QMainWindow, Ui_MainWindow):
     payment_signal = QtCore.pyqtSignal()
     back_signal = QtCore.pyqtSignal()
     order_signal = QtCore.pyqtSignal()
-
-
-    def __init__(self):
-        super().__init__()
+    menu_signal = QtCore.pyqtSignal()
+    def __init__(self, parent=None):
+        super(posTable, self).__init__(parent)
         self.setupUi(self)
+        self.initUI()
+        self.firstFloorTableStatus = [True] * 6  # True means available
+        self.secondFloorTableStatus = [True] * 6  # True means available
 
-        self.paymentBTN.clicked.connect(self.navigate_payment)
-        self.backBTN.clicked.connect(self.back)
-        self.orderBTN.clicked.connect(self.navigate_order)
+    def initUI(self):
+        # Connect buttons to their functions
+        self.homeBTN.clicked.connect(self.goHome)
+        self.menuBTN.clicked.connect(self.goMenu)
+        self.paymentBTN.clicked.connect(self.goPayment)
+        self.orderBTN.clicked.connect(self.goOrder)
+        self.backBTN.clicked.connect(self.goBack)
+        self.firstFloorBTN.clicked.connect(self.showFirstFloorTables)
+        self.secondFloorBTN.clicked.connect(self.showSecondFloorTables)
+        self.tableOneButton.clicked.connect(lambda: self.selectTable(1))
+        self.tableTwoButton.clicked.connect(lambda: self.selectTable(2))
+        self.tableThreeButton.clicked.connect(lambda: self.selectTable(3))
+        self.tableFourButton.clicked.connect(lambda: self.selectTable(4))
+        self.tableFiveButton.clicked.connect(lambda: self.selectTable(5))
+        self.tableSixButton.clicked.connect(lambda: self.selectTable(6))
 
-        # Create a QTimer object
-        self.timer = QTimer()
 
-        # Connect the timeout signal of the timer to the updateDateTime slot
-        self.timer.timeout.connect(self.updateDateTime)
+    def goHome(self):
+        self.home_signal.emit()
 
-        # Set the interval for the timer (in milliseconds)
-        self.timer.start(1000)  # Update every second
+    def goMenu(self):
+        self.menu_signal.emit()
 
-    def updateDateTime(self):
-        # Get the current date and time
-        currentDateTime = QDateTime.currentDateTime()
-
-        # Format the date and time together as desired
-        formattedDateTime = currentDateTime.toString("MMMM d, yyyy, hh:mm:ss AP")
-
-        # Set the text of dateLabel to the formatted date and time
-        self.label_2.setText(formattedDateTime)
-
-    def navigate_payment(self):
+    def goPayment(self):
         self.payment_signal.emit()
 
-    def back(self):
+    def goOrder(self):
+        self.order_signal.emit()
+
+    def goBack(self):
         self.back_signal.emit()
 
-    def navigate_order(self):
-        self.order_signal.emit()
+    def showFirstFloorTables(self):
+        self.firstFloorBTN.setStyleSheet(
+            "background-color: #67B99A; color: white; padding: 8px 16px; border-radius: 5px;")
+        self.secondFloorBTN.setStyleSheet(
+            "background-color: #FAFAFA; color: #D9D9D9; padding: 8px 16px; border-radius: 5px;")
+        print("First Floor Tables")
+
+        self.tableOneButton.setText("Table 1")
+        self.tableTwoButton.setText("Table 2")
+        self.tableThreeButton.setText("Table 3")
+        self.tableFourButton.setText("Table 4")
+        self.tableFiveButton.setText("Table 5")
+        self.tableSixButton.setText("Table 6")
+
+        self.updateTableStatus(self.firstFloorTableStatus)
+
+        self.tableOneButton.clicked.disconnect()
+        self.tableTwoButton.clicked.disconnect()
+        self.tableThreeButton.clicked.disconnect()
+        self.tableFourButton.clicked.disconnect()
+        self.tableFiveButton.clicked.disconnect()
+        self.tableSixButton.clicked.disconnect()
+
+        self.tableOneButton.clicked.connect(lambda: self.selectTable(1))
+        self.tableTwoButton.clicked.connect(lambda: self.selectTable(2))
+        self.tableThreeButton.clicked.connect(lambda: self.selectTable(3))
+        self.tableFourButton.clicked.connect(lambda: self.selectTable(4))
+        self.tableFiveButton.clicked.connect(lambda: self.selectTable(5))
+        self.tableSixButton.clicked.connect(lambda: self.selectTable(6))
+
+    def showSecondFloorTables(self):
+        self.secondFloorBTN.setStyleSheet(
+            "background-color: #67B99A; color: white; padding: 8px 16px; border-radius: 5px;")
+        self.firstFloorBTN.setStyleSheet(
+            "background-color: #FAFAFA; color: #D9D9D9; padding: 8px 16px; border-radius: 5px;")
+        print("Second Floor Tables")
+
+        self.tableOneButton.setText("Table 7")
+        self.tableTwoButton.setText("Table 8")
+        self.tableThreeButton.setText("Table 9")
+        self.tableFourButton.setText("Table 10")
+        self.tableFiveButton.setText("Table 11")
+        self.tableSixButton.setText("Table 12")
+
+        self.updateTableStatus(self.secondFloorTableStatus)
+
+        self.tableOneButton.clicked.disconnect()
+        self.tableTwoButton.clicked.disconnect()
+        self.tableThreeButton.clicked.disconnect()
+        self.tableFourButton.clicked.disconnect()
+        self.tableFiveButton.clicked.disconnect()
+        self.tableSixButton.clicked.disconnect()
+
+        self.tableOneButton.clicked.connect(lambda: self.selectSecondFloorTable(7))
+        self.tableTwoButton.clicked.connect(lambda: self.selectSecondFloorTable(8))
+        self.tableThreeButton.clicked.connect(lambda: self.selectSecondFloorTable(9))
+        self.tableFourButton.clicked.connect(lambda: self.selectSecondFloorTable(10))
+        self.tableFiveButton.clicked.connect(lambda: self.selectSecondFloorTable(11))
+        self.tableSixButton.clicked.connect(lambda: self.selectSecondFloorTable(12))
+
+    def updateTableStatus(self, tableStatus):
+        self.tableOneButton.setEnabled(True)
+        self.tableTwoButton.setEnabled(True)
+        self.tableThreeButton.setEnabled(True)
+        self.tableFourButton.setEnabled(True)
+        self.tableFiveButton.setEnabled(True)
+        self.tableSixButton.setEnabled(True)
+
+        # Update labels for each table
+        self.updateTableLabels(tableStatus)
+
+    def updateTableLabels(self, tableStatus):
+        labels = [self.tableOneLBL, self.tableTwoLBL, self.tableThreeLBL, self.tableFourLBL, self.tableFiveLBL,
+                  self.tableSixLBL]
+        buttons = [self.tableOneButton, self.tableTwoButton, self.tableThreeButton, self.tableFourButton,
+                   self.tableFiveButton, self.tableSixButton]
+
+        for i, status in enumerate(tableStatus):
+            if status:
+                labels[i].setText("Available")
+                buttons[i].setStyleSheet("background-color: #67B99A; color: white;")
+            else:
+                labels[i].setText("Unavailable")
+                buttons[i].setStyleSheet("background-color: #D9D9D9; color: white;")
+
+    def selectTable(self, table_number):
+        if self.firstFloorTableStatus[table_number - 1]:
+            guess, ok = QtWidgets.QInputDialog.getInt(self, f"Table {table_number}",
+                                                      "Enter the number of guests (1-4):",
+                                                      min=1, max=4)
+            if ok:
+                self.guestNum.setText(str(guess))
+                self.tableNum.setText(f"{table_number}")
+                print(f"Table {table_number} selected with {guess} guests")
+                self.firstFloorTableStatus[table_number - 1] = False  # Mark table as unavailable
+                self.updateTableStatus(self.firstFloorTableStatus)
+        else:
+            QtWidgets.QMessageBox.information(self, "Table Unavailable",
+                                              f"Table {table_number} is currently unavailable.")
+
+    def selectSecondFloorTable(self, table_number):
+        if self.secondFloorTableStatus[table_number - 7]:
+            guess, ok = QtWidgets.QInputDialog.getInt(self, f"Table {table_number}",
+                                                      "Enter the number of guests (1-4):",
+                                                      min=1, max=4)
+            if ok:
+                self.guestNum.setText(str(guess))
+                self.tableNum.setText(f"{table_number}")
+                print(f"Table {table_number} selected with {guess} guests")
+                self.secondFloorTableStatus[table_number - 7] = False  # Mark table as unavailable
+                self.updateTableStatus(self.secondFloorTableStatus)
+        else:
+            QtWidgets.QMessageBox.information(self, "Table Unavailable",
+                                              f"Table {table_number} is currently unavailable.")
+
+
+if __name__ == "__main__":
+    app = QtWidgets.QApplication(sys.argv)
+    mainWin = posTable()
+    mainWin.show()
+    sys.exit(app.exec_())
