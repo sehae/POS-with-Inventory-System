1c2dce89ed7fe5ba418c46360ac0aac9868e404d
sensehae <106131457+sehae@users.noreply.github.com>
2024-06-14 01:35:12 +0800

    Update DB_Queries.py

--- database/DB_Queries.py
+++ database/DB_Queries.py
@@ -17,8 +17,8 @@ GET_NEXT_ADMIN_ID = "SELECT MAX(admin_id) FROM admin;"
 GET_NEXT_EMPLOYEE_ID = "SELECT MAX(employee_id) FROM employee;"
 
 # Update Queries
-UPDATE_ADMIN_PASSWORD = "UPDATE admin SET password = %s WHERE email = %s;"
-UPDATE_EMPLOYEE_PASSWORD = "UPDATE employee SET password = %s WHERE email = %s;"
+UPDATE_ADMIN_PASSWORD = "UPDATE admin SET password = %s WHERE username = %s;"
+UPDATE_EMPLOYEE_PASSWORD = "UPDATE employee SET password = %s WHERE username = %s;"
 
 # Add User Queries
 ADD_ADMIN = ("INSERT INTO admin (last_name, first_name, contact_number, email, username, password) VALUES (%s, %s, %s, "
@@ -48,7 +48,7 @@ DISABLE_EMPLOYEE = "UPDATE employee SET is_active = False WHERE email = %s;"
 # Search User Query
 SEARCH_EMPLOYEE = ("SELECT first_name, last_name, email, department FROM employee WHERE (last_name LIKE %s OR "
                    "first_name LIKE %s OR email LIKE %s) AND is_active = True;")
-SEARCH_ADMIN = ("SELECT first_name, last_name, email FROM admin WHERE (last_name LIKE %s OR first_name LIKE %s OR "
+SEARCH_ADMIN = ("SELECT first_name, last_name, email, 'Admin' FROM admin WHERE (last_name LIKE %s OR first_name LIKE %s OR "
                 "email LIKE %s) AND is_active = True;")
 
 # User Logs
