e4f79851ab8253d0e2a7027c57673c3dc4ae95ee
sensehae <106131457+sehae@users.noreply.github.com>
2024-06-13 21:53:53 +0800

    idk?

--- shared/imports.py
+++ shared/imports.py
@@ -14,12 +14,11 @@ from PyQt5.QtCore import QRegExp
 # Database
 from server.local_server import conn
 from database.DB_Queries import (GET_ADMIN_LOGIN, GET_ADMIN_FIRST_NAME, GET_EMPLOYEE_LOGIN,
-                                 GET_EMPLOYEE_FIRST_NAME, GET_ADMIN_ID, GET_EMPLOYEE_ID, UPDATE_ADMIN_PASSWORD,
-                                 UPDATE_EMPLOYEE_PASSWORD, ADD_ADMIN, ADD_EMPLOYEE, ADD_ADMIN_LOGIN, ADD_EMPLOYEE_LOGIN,
+                                 GET_EMPLOYEE_FIRST_NAME, GET_ADMIN_ID, UPDATE_ADMIN_PASSWORD,
+                                 UPDATE_EMPLOYEE_PASSWORD, ADD_ADMIN, ADD_EMPLOYEE,
                                  GET_ADMIN_DATA, GET_EMPLOYEE_DATA, MOVE_TO_ADMIN, MOVE_TO_EMPLOYEE,
                                  UPDATE_EMPLOYEE_DEPARTMENT, ENABLE_ADMIN, ENABLE_EMPLOYEE, DISABLE_ADMIN,
-                                 DISABLE_EMPLOYEE, SEARCH_EMPLOYEE, SEARCH_ADMIN, DISABLE_ADMIN_ID,
-                                 DISABLE_ACCOUNT_EMPLOYEE, DISABLE_ACCOUNT_ADMIN,)
+                                 DISABLE_EMPLOYEE, SEARCH_EMPLOYEE, SEARCH_ADMIN)
 
 # Automated Email
 from automated.email_automation import send_username_password
@@ -37,5 +36,4 @@ from shared.dialog import (show_error_message, show_username_password, confirmat
 # Styles
 from styles.universalStyles import (COMBOBOX_STYLE, COMBOBOX_STYLE_VIEW, COMBOBOX_DISABLED_STYLE,
                                     ACTIVE_BUTTON_STYLE, INACTIVE_BUTTON_STYLE, )
-
 
