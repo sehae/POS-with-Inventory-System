cc4248c9d282851804eb1c26a74456ffb726a193
sehae <ldbvillatura@yahoo.com>
2024-06-16 15:58:28 +0800

    Update email_automation.py

--- automated/email_automation.py
+++ automated/email_automation.py
@@ -28,3 +28,30 @@ def send_username_password(username, password, email):
     server.send_message(msg)
 
     print('Email Sent')
+
+
+# Account Update
+def send_new_details(username, email):
+    msg = EmailMessage()
+    msg['Subject'] = 'MOON HEY HOTPOT - WE HAVE UPDATED YOUR LOGIN DETAILS!'
+    msg['From'] = from_mail
+    msg['To'] = email
+
+    message = f"""
+    Hello,
+
+    We have updated your login details, here are your new login details:
+
+    Username: {username}
+    Password: still the same as your previous password.
+
+    Disregard the previous login details, they are no longer valid.
+    Please do not share these details with anyone. You are free to change your password once you log in.
+
+    Best,
+    Moon Hey Hotpot and Grill Team
+    """
+
+    msg.set_content(message)
+    server.send_message(msg)
+    print('Email Sent')
